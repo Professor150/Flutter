4e9f4363f3dcda8543538464418f1cf2bbdc56da
Pradeep Tharu <pradeeptharu535@gmail.com>
2022-10-15 04:46:20 +0300

    transaction File Added

---
+++ lib/widgets/models/transaction.dart
@@ -0,0 +1,13 @@
+class Translation {
+  final String id;
+  final String name;
+  final double amount;
+  final DateTime date;
+
+  Translation({
+    required this.id,
+    required this.name,
+    required this.amount,
+    required this.date,
+  });
+}
