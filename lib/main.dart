41dbd69528bf64d7354d1e05a6d3f7487cf73d42
Pradeep Tharu <pradeeptharu535@gmail.com>
2022-10-15 04:41:08 +0300

    Homepage Design

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/homepage.dart';
+
+void main() => runApp(facebook());
+
+class facebook extends StatelessWidget {
+  const facebook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/widgets/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}
