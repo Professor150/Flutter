4f3fe7ce2deda07a608499d818b3e3908453b98c
Pradeep Tharu <pradeeptharu535@gmail.com>
2022-10-26 05:49:14 +0300

    Project Almost Completed

--- lib/widgets/homepage.dart
+++ lib/widgets/homepage.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/chart.dart';
 import 'package:flutter_application_1/widgets/transaction_list.dart';
 import 'package:intl/intl.dart';
 import '/widgets/models/transaction.dart';
 import 'newtransaction.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
   @override
   State<HomePage> createState() => HomePageState();
 }
@@ -18,6 +17,16 @@ class HomePageState extends State<HomePage> {
   final List<Transaction> _userTransaction = [
     // Transaction(id: '1', name: 'Pradeep  ', amount: 100, date: DateTime.now()),
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransaction.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
@@ -71,31 +80,14 @@ class HomePageState extends State<HomePage> {
                 begin: Alignment.bottomCenter,
                 end: Alignment.centerRight,
                 colors: <Color>[Colors.blue, Colors.green.shade300])),
-        child: Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  height: 50,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.yellow.shade100),
-                  child: Center(
-                    child: Text(
-                      ' TO DO APP',
-                      style: TextStyle(
-                        fontSize: 30,
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TransactionList(_userTransaction),
-              ],
-            ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Chart(_recentTransactions),
+              SizedBox(height: 10),
+              TransactionList(_userTransaction),
+            ],
           ),
         ),
       ),
---
+++ lib/widgets/models/style_calss.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle myText20() {
+  return new TextStyle(
+    fontFamily: GoogleFonts.lato().fontFamily,
+    fontSize: 20,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+  );
+}
+
+TextStyle myText25() {
+  return new TextStyle(
+    fontFamily: GoogleFonts.lato().fontFamily,
+    fontSize: 25,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+  );
+}
+
+TextStyle myText30() {
+  return new TextStyle(
+    fontFamily: GoogleFonts.lato().fontFamily,
+    fontSize: 30,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+  );
+}
+
+TextStyle myText35() {
+  return new TextStyle(
+    fontFamily: GoogleFonts.lato().fontFamily,
+    fontSize: 35,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+  );
+}
+
+TextStyle myText40() {
+  return new TextStyle(
+    fontFamily: GoogleFonts.lato().fontFamily,
+    fontSize: 40,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+  );
+}
